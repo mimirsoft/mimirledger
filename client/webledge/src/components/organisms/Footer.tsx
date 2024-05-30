911bf544a22b4b9a43d3aa0215aeb89563ac269d
caoster <milhoan@gmail.com>
2024-05-29 19:18:20 -0700

    wip update footer

--- client/webledge/src/components/organisms/Footer.tsx
+++ client/webledge/src/components/organisms/Footer.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Footer() {
     return (<>
         <div className="footer" >
-            Updated 28 May 2024 - Site Designed and Built by <a href="http://www.mimirsoft.com">Mimir Software Corp</a> &copy; <br />
+            Updated 28 May 2024 - Designed and Built by <a href="http://www.mimirsoft.com">Mimir Software Corp</a> &copy; <br />
         </div>
     </>)
 }
